070390089bc470249464a4799a23555186199ef6
Mateo Diet <157379509+IMatAK42@users.noreply.github.com>
2026-02-05 12:15:40 +0100

    Correct CI/CD pipeline and Jacoco Config

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       suppressAll: true
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pmt-frontend'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
